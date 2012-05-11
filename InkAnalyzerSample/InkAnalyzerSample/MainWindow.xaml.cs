b87a4ab63e90c7da06b4a2228d4b8c6fbad260f6
kaorun55 <kaorun55@gmail.com>
2012-05-11 14:52:03 +0900

    手書き認識のサンプルを追加

---
+++ InkAnalyzerSample/InkAnalyzerSample/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+
+namespace InkAnalyzerSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnalyze_Click(object sender, RoutedEventArgs e)
+        {
+            InkAnalyzer theInkAnalyzer = new InkAnalyzer();
+
+            // キャンバスに描かれた文字を認識するためにアナライザにストロークをセット
+            theInkAnalyzer.AddStrokes(InkCanvas1.Strokes);
+            theInkAnalyzer.SetStrokesType(InkCanvas1.Strokes, StrokeType.Writing);
+
+            // 文字を解析
+            theInkAnalyzer.Analyze();
+
+            // 文字を解析した結果の第1候補を表示する
+            MessageBox.Show(theInkAnalyzer.GetRecognizedString());
+
+            // その他の候補を表示する
+            AnalysisAlternateCollection alternates = theInkAnalyzer.GetAlternates();
+            foreach (var alternate in alternates)
+                MessageBox.Show(alternate.RecognizedString);
+        }
+    }
+}
